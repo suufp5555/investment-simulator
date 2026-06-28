1a380d062e6915e95e58149d45a70324f83c7a85
suufp5555 <suufp5555@gmail.com>
2026-06-28 14:01:52 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/investment-simulator/', // ★リポジトリ名と同じ名前を「/」で挟んで追加
+  base: '/investment-simulator/', 
 })
